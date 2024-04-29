50819583a89342f647d5b5059ffaa3c75196aba8
Kyle <Archaon@byui.edu>
2024-04-29 16:31:53 -0600

    prep 3 done part of prep 4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
-    }
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        int guess = 0;
+        do{
+            // Console.WriteLine($"the magic number is: {magicNumber}");
+            Console.Write("Guess a number: ");
+            guess = Convert.ToInt32(Console.ReadLine());
+            if (guess > magicNumber) {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber){
+                Console.WriteLine("Higher");
+            }
+
+
+        } while(magicNumber != guess);
+
+
+    }       
 }
+    
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+
+        Console.WriteLine("Enter a list of numbers type 0 when done");
+        List<int> numbers = new List<int>();
+        int newNumber = 0;
+        int maxNum = 0;
+        do {
+            Console.Write("Enter number: ");
+            newNumber = Convert.ToInt32(Console.ReadLine());
+            numbers.Add(newNumber);
+            if (newNumber > maxNum)
+            {
+                maxNum = newNumber;
+            }
+
+        } while (newNumber !=0);
+
+        int sum = 0;
+        int numCount = 0;
+
+        foreach(int number in numbers)
+        {
+            sum += number;
+            numCount ++;
+            
+        }
+        Console.WriteLine($"The total of the list is: {sum}");
+        int avgNum = sum / numCount; 
+        Console.WriteLine($"The average number is: {avgNum}");
+        Console.WriteLine($"The largest number is: {maxNum}");
+
     }
 }
