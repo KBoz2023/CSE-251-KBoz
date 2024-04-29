6b7f18b3b0519eaef87380f6ba9523952c1e938c
Kyle <Archaon@byui.edu>
2024-04-29 16:33:25 -0600

    changes

---
+++ demo.csx
@@ -0,0 +1,5 @@
+using System;
+
+
+string ageAsString = Console.ReadLine(Console.Write("Enter your age: "));
+int age = Convert.ToInt32(ageAsString);
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.Write("Do you want to play a game: ");
     }
 }
